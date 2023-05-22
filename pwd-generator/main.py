e98a53ebef433ea484ace171a59cc14b88e50ae6
Elena-Weber <crochetmiracles@gmail.com>
2023-05-22 19:16:01 -0400

    update pwd generator

--- pwd-generator/main.py
+++ pwd-generator/main.py
@@ -4,22 +4,22 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the PyPassword Generator! I will help you generate a password. Let us start!")
 number_of_letters = int(input("How many letters would you like in your password?\n")) 
 number_of_symbols = int(input(f"How many symbols would you like?\n"))
 number_of_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level
+#Easy Level
 # password = ""
 
 # for char in range(1, number_of_letters + 1):
-#   password += random.choice(letters)
+#     password += random.choice(letters)
 
 # for char in range(1, number_of_symbols + 1):
-#   password += random.choice(symbols)
+#     password += random.choice(symbols)
 
 # for char in range(1, number_of_numbers + 1):
-#   password += random.choice(numbers)
+#     password += random.choice(numbers)
 
 # print(password)
 
@@ -27,13 +27,13 @@ number_of_numbers = int(input(f"How many numbers would you like?\n"))
 password_list = []
 
 for char in range(1, number_of_letters + 1):
-  password_list.append(random.choice(letters))
+    password_list.append(random.choice(letters))
 
 for char in range(1, number_of_symbols + 1):
-  password_list += random.choice(symbols)
+    password_list += random.choice(symbols)
 
 for char in range(1, number_of_numbers + 1):
-  password_list += random.choice(numbers)
+    password_list += random.choice(numbers)
 
 print(password_list)
 random.shuffle(password_list)
@@ -41,6 +41,6 @@ print(password_list)
 
 password = ""
 for char in password_list:
-  password += char
+    password += char
 
 print(f"Your password is: {password}")
