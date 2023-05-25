747d3aabbe31d0141cd7b93156dd39a4cbde8c14
Elena-Weber <crochetmiracles@gmail.com>
2023-05-25 17:57:16 -0400

    add quiz

---
+++ quiz/main.py
@@ -0,0 +1,20 @@
+from question import Question
+from questions import questions
+from quiz_brain import QuizBrain
+
+print("\nWelcome to PyQuiz! Answer 10 questions and be considered a genius!\n")
+
+question_bank = []
+for question in questions:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("Thanks for playing!")
+print(f"Your final score is {quiz.score} out of {quiz.question_number}")
---
+++ quiz/questions.py
@@ -0,0 +1,72 @@
+questions = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Was the HTML5 standard published in 2014?",
+        "correct_answer": "Y"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Was the first computer bug formed by faulty wires?",
+        "correct_answer": "N"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Does FLAC stand for 'Free Lossless Audio Condenser'?",
+        "correct_answer": "N"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Do all program codes have to be compiled into an executable file in order to be run an can this file then be executed on any machine?",
+        "correct_answer": "N"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Did Linus Torvalds create Linux and Git?",
+        "correct_answer": "Y"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Is the programming language 'Python' based off a modified version of 'JavaScript'?",
+        "correct_answer": "N"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Did AMD create the first consumer 64-bit processor?",
+        "correct_answer": "Y"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Does 'HTML' stand for Hypertext Markup Language?",
+        "correct_answer": "Y"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In most programming languages, is the operator ++ equivalent to the statement '+= 1'?",
+        "correct_answer": "Y"
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "Did the IBM PC use an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory?",
+        "correct_answer": "N"
+    }
+]
